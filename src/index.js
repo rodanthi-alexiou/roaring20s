700c0b9642e6c7d772317ecc332bcff682d69552
rodanthi-alexiou <61449673+rodanthi-alexiou@users.noreply.github.com>
2021-08-14 20:59:57 +0300

    new

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import HashRouter from "hash-router";
+import {  BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <Router>
       <App />
-    </HashRouter>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
