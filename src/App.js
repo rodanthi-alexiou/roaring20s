9c3f3382eee98eafb590eec8565316e9230db476
rodanthi-alexiou <61449673+rodanthi-alexiou@users.noreply.github.com>
2021-08-15 13:11:26 +0300

    test

--- src/App.js
+++ src/App.js
@@ -16,46 +16,25 @@ import BookPost from './BookPost'
 function App() {
   return (
 
-    
-                
-                <Router >
-
-                <Switch>
-                  <Route
-                    exact
-                    path="/roaring20s/" render={() => {
-                        return(
+    <body>
                           
-                          <body>
-                          
-                          <div class="box">
-                          <div class="topnav">
-                          <a href="/roaring20s/booksrec">Book Recs</a>
-                          <a href="/roaring20s/jams">Jams</a>
-                          <a>Coffee Space</a>
-                        </div>
-        
-                          <div class="header">
-                            <h1>Roaring 20s</h1>
-                            <p>No one knows what they're doing either</p>
-                            <img src={"https://i.pinimg.com/564x/e2/57/d4/e257d41074880d5c51c326b994110639.jpg"} class="image"/>
-                            <p>and it's okay</p>
-                          </div>
+    <div class="box">
+    <div class="topnav">
+    <a href="/roaring20s/booksrec">Book Recs</a>
+    <a href="/roaring20s/jams">Jams</a>
+    <a>Coffee Space</a>
+  </div>
+
+    <div class="header">
+      <h1>Roaring 20s</h1>
+      <p>No one knows what they're doing either</p>
+      <img src={"https://i.pinimg.com/564x/e2/57/d4/e257d41074880d5c51c326b994110639.jpg"} class="image"/>
+      <p>and it's okay</p>
+    </div>
 
 
-                          </div>
-                        </body>
-                        );
-                      }}/>
-        
-        
-
-                  <Route path="/roaring20s/booksrec" component={BooksRec}/>
-                  <Route path="/roaring20s/jams" component={Jams}/>
-                  <Route path="/roaring20s/bookpost" component={BookPost}/>
-                </Switch>
-              </Router>
-              
+    </div>
+  </body>     
 
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {  BrowserRouter as Router } from "react-router-dom";
+import HashRouter from "hash-router";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    
       <App />
-    </Router>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
