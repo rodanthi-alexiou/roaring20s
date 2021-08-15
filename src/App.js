94a7abdd059b5deb9792c953aa47f2f78b9b556b
rodanthi-alexiou <61449673+rodanthi-alexiou@users.noreply.github.com>
2021-08-15 15:34:16 +0300

    new

--- src/App.js
+++ src/App.js
@@ -16,25 +16,40 @@ import BookPost from './BookPost'
 function App() {
   return (
 
-    <body>
-                          
-    <div class="box">
-    <div class="topnav">
-    <a href="/roaring20s/booksrec">Book Recs</a>
-    <a href="/roaring20s/jams">Jams</a>
-    <a>Coffee Space</a>
-  </div>
+    <Switch>
+    <Route
+      exact
+      path="/roaring20s/" render={() => {
+          return(
+            
+            <body>
+            
+            <div class="box">
+            <div class="topnav">
+            <a href="/roaring20s/booksrec">Book Recs</a>
+            <a href="/roaring20s/jams">Jams</a>
+            <a>Coffee Space</a>
+          </div>
 
-    <div class="header">
-      <h1>Roaring 20s</h1>
-      <p>No one knows what they're doing either</p>
-      <img src={"https://i.pinimg.com/564x/e2/57/d4/e257d41074880d5c51c326b994110639.jpg"} class="image"/>
-      <p>and it's okay</p>
-    </div>
+            <div class="header">
+              <h1>Roaring 20s</h1>
+              <p>No one knows what they're doing either</p>
+              <img src={"https://i.pinimg.com/564x/e2/57/d4/e257d41074880d5c51c326b994110639.jpg"} class="image"/>
+              <p>and it's okay</p>
+            </div>
 
 
-    </div>
-  </body>     
+            </div>
+          </body>
+          );
+        }}/>
+
+
+
+    <Route path="/roaring20s/booksrec" component={BooksRec}/>
+    <Route path="/roaring20s/jams" component={Jams}/>
+    <Route path="/roaring20s/bookpost" component={BookPost}/>
+  </Switch>     
 
   );
 }
